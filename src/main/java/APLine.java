570ff217adb25a9035bfbde77f85f3ad5218f725
kaluu554 <143027760+kaluu554@users.noreply.github.com>
2024-04-22 12:34:00 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,18 +1,18 @@
 public class APLine
 {
-  private int a1, b1, c1;
-  public APLine(int a, int b, int c){
-    a1 = a;
-    b1 = b;
-    c1 = c;
+  private int a, b, c;
+  public APLine(int a1, int b1, int c1){
+    a = a1;
+    b = b1;
+    c = c1;
   }
 
   public double getSlope(){
-    return -(double)a1/b1;
+    return -(double)a/b;
     
   }
 
   public boolean isOnline(int x,int y){
-    return a1*x + b1*y + c1 == 0;
+    return a*x + b*y + c == 0;
   }
 }
